92f18595d5bc0e1be3de7a38198aea5a6b31c03b
kimmeh <mhko0411@gmail.com>
2023-01-07 14:51:39 +0900

    feat: lazy load image component

---
+++ src/components/LazyLoadImg/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export interface LazyLoadImgProps {
+  src: string;
+  alt: string;
+}
+
+export default function LazyLoadImg({ src, alt }: LazyLoadImgProps) {
+  const imgRef = useRef(null);
+  const [isView, setIsView] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (imgRef === null || imgRef.current === null) {
+      return;
+    }
+    const imgObserver = new IntersectionObserver((entries, observer) => {
+      const [entry] = entries;
+
+      if (entry.isIntersecting) {
+        setIsView(true);
+        observer.unobserve(entry.target);
+      }
+    });
+
+    imgObserver.observe(imgRef.current);
+  }, [imgRef]);
+
+  return <img src={isView ? src : ""} alt={alt} ref={imgRef} />;
+}
